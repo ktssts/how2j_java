c7a9728c7d80f198d1640e8613be704122391d31
ktssts <ub.songwenxu@gmail.com>
2021-12-03 17:11:37 +0900

    2021.12.03 list practice3

--- src/base_4_list/listp2/Main.java
+++ src/base_4_list/listp2/Main.java
@@ -1,4 +1,23 @@
 package base_4_list.listp2;
 
+import java.util.Arrays;
+
 public class Main {
+    public static void main(String[] args) {
+        int [] list = new int[5];
+        for (int i =0;i< list.length;i++){
+            list[i] = (int) (Math.random()*100);
+        }
+        System.out.println(Arrays.toString(list));
+
+        int[] listB = new int[5];
+        int k = 4;
+        for (int i = 0; i<list.length;i++){
+
+            listB[k]=list[i];
+            k--;
+        }
+        System.out.println(Arrays.toString(listB));
+    }
+
 }
